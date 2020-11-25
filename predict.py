271e663ad1a9021a80206c3725d16e3b5ba7a404
unknown <tsuguro4649@gmail.com>
2020-11-25 22:02:22 +0900

    update

--- predict.py
+++ predict.py
@@ -1,9 +1,8 @@
-from keras.models import Sequential
+from keras.models import Sequential, load_model
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
 from keras.utils import np_utils
-import keras
-import tensorflow
+import keras, sys, tensorflow
 import numpy as np
 from PIL import Image
 
@@ -42,3 +41,22 @@ def build_model():
     model = load_model('./animal_cnn_aug.h5')
 
     return model
+
+
+def main():
+    image = Image.open(sys.argv[1])
+    image = image.convert('RGB')
+    image = image.resize((image_size, image_size))
+    data = np.asarray(image)
+    X = []
+    X.append(data)
+    X = np.array(X)
+    model = build_model()
+
+    result = model.predict([X])[0]
+    predicted = result.argmax()
+    percentage = int(result[predicted] * 100)
+    print("{0}({1} %)".format(classes[predicted], percentage))
+
+if __name__ == "__main__":
+    main()
